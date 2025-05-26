22cd993906bf542162006b8c5296f7bf0fa398a4
Tobeadragon <mastertyj@hotmail.com>
2025-05-26 13:50:12 +0200

    chore: デフォルトのfaviconを非表示に設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,6 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Tatsuya Yamashita - Full-stack Developer' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ]
     }
   }
