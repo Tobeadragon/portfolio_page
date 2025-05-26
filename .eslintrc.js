4112dcdfed9d64f8bcb9e884a5b51abfe973f344
Tobeadragon <mastertyj@hotmail.com>
2025-05-26 09:37:05 +0200

    chore: ESLintとPrettierの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        '@nuxtjs/eslint-config-typescript',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended',
+    ],
+    plugins: ['prettier'],
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    },
+} 
